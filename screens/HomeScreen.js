b80e5a38e46513c546848e5a85e3371dc3873afe
IT4GOD <102861520+it4god@users.noreply.github.com>
2023-10-17 16:53:34 +0700

    Layout Revision 1

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,22 +3,21 @@ import { View, Text, StyleSheet, TouchableOpacity, Alert, Dimensions, ScrollView
 import { GiftedChat, InputToolbar, Send } from 'react-native-gifted-chat';
 import {
   Icon,
-  IconElement,
   Layout,
-  MenuItem,
-  OverflowMenu,
   TopNavigation,
-  TopNavigationAction,
-  BottomNavigation,
-  BottomNavigationTab,
   Avatar,
   Divider,
-
+  Spinner,
+  TopNavigationAction,
+  MenuItem,
+  OverflowMenu,
+  renderMenuAction
 } from '@ui-kitten/components';
-
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
-const CHAT_GPT_API_KEY = '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'
+import Config from "react-native-config";
+import Tts from "react-native-tts";
+import Voice from 'react-native-voice';
 const user = {
   _id: 1,
   name: 'User',
@@ -31,26 +30,159 @@ class HomeScreen extends React.Component {
     super(props)
     this.state = {
       messages: [],
-      userMessages: []
+      userMessages: [],
+      recognized: '',
+      pitch: '',
+      error: '',
+      end: '',
+      started: '',
+      results: [],
+      partialResults: [],
+      isTyping: false,
+      menuVisible: false,
+      sound : true,
+    }
+  }
+  componentDidMount() {
+    Tts.setDefaultLanguage('id-ID');
+    Voice.onSpeechStart = this.onSpeechStart;
+    Voice.onSpeechRecognized = this.onSpeechRecognized;
+    Voice.onSpeechEnd = this.onSpeechEnd;
+    Voice.onSpeechError = this.onSpeechError;
+    Voice.onSpeechResults = this.onSpeechResults;
+    Voice.onSpeechPartialResults = this.onSpeechPartialResults;
+    Voice.onSpeechVolumeChanged = this.onSpeechVolumeChanged;
+  }
+  onSpeechStart = (e) => {
+    console.log('onSpeechStart: ', e);
+    this.setState({
+      started: '√',
+    });
+  };
+
+  onSpeechRecognized = (e) => {
+    console.log('onSpeechRecognized: ', e);
+    this.setState({
+      recognized: '√',
+    });
+  };
+
+  onSpeechEnd = (e) => {
+    console.log('onSpeechEnd: ', e);
+    this.setState({
+      end: '√',
+    });
+  };
+
+  onSpeechError = (e) => {
+    console.log('onSpeechError: ', e);
+    this.setState({
+      error: JSON.stringify(e.error),
+    });
+  };
+
+  onSpeechResults = (e) => {
+    console.log('onSpeechResults: ', e);
+    this.setState({
+      results: e.value,
+    });
+    const chatMessage = [
+      {
+        _id: Math.random().toString(36).substring(7),
+        text: e.value[0],
+        createdAt: new Date(),
+        user: {
+          _id: 1,
+          name: 'User',
+          avatar: require('../assets/profile.png'),
+        },
+      },
+    ];
+    this.onSend(chatMessage)
+  };
+
+  onSpeechPartialResults = (e) => {
+    console.log('onSpeechPartialResults: ', e);
+    this.setState({
+      partialResults: e.value,
+    });
+  };
+
+  onSpeechVolumeChanged = (e) => {
+    console.log('onSpeechVolumeChanged: ', e);
+    this.setState({
+      pitch: e.value,
+    });
+
+
+  };
+
+  _StartRecognizing = async () => {
+    this.setState({
+      recognized: '',
+      pitch: '',
+      error: '',
+      started: '',
+      results: [],
+      partialResults: [],
+      end: '',
+    });
+    try {
+      Voice.start('id-ID');
+    } catch (e) {
+      console.error(e);
     }
   }
 
+  _stopRecognizing = async () => {
+    try {
+      await Voice.stop();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  _cancelRecognizing = async () => {
+    try {
+      await Voice.cancel();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  _destroyRecognizer = async () => {
+    try {
+      await Voice.destroy();
+    } catch (e) {
+      console.error(e);
+    }
+    this.setState({
+      recognized: '',
+      pitch: '',
+      error: '',
+      started: '',
+      results: [],
+      partialResults: [],
+      end: '',
+
+    });
+  };
+
   sendMessage = async (message) => {
-    console.log(message)
+    this.setState({ isTyping: true })
     try {
       const openaiEndpoint = 'https://api.openai.com/v1/chat/completions'
-      const prompt = message;
       const userMessage = { role: 'user', content: message };
+      const systemMessage = { role: 'system', content: 'Kamu adalah Bible Man. Segala respon dan pengetahuanmu harus didasarkan pada Alkitab. Gunakan bahasa yang ramah dan alkitabiah' };
       const data = {
-        max_tokens: 100,
-        n: 1,
-        stop: '\n',
+        max_tokens: 2000,
+        temperature:0.7,
         model: 'gpt-3.5-turbo',
-        messages: [...this.state.userMessages, userMessage],
+        messages: [systemMessage,...this.state.userMessages, userMessage],
       };
       const headers = {
         'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + CHAT_GPT_API_KEY,
+        Authorization: 'Bearer ' + Config.CHAT_GPT_API_KEY,
       };
       const response = await fetch(openaiEndpoint, {
         method: 'POST',
@@ -58,7 +190,7 @@ class HomeScreen extends React.Component {
         body: JSON.stringify(data),
       });
       const json = await response.json();
-      console.log(json.choices[0].message.content)
+      this.setState({ isTyping: false })
       return json.choices[0].message.content;
 
     } catch (err) {
@@ -67,7 +199,7 @@ class HomeScreen extends React.Component {
   }
 
   onSend = async (newMessages = []) => {
-
+    Tts.stop();
 
     const chatMessageUser = [
       {
@@ -84,8 +216,9 @@ class HomeScreen extends React.Component {
     this.setState(previousState => ({
       messages: GiftedChat.append(previousState.messages, chatMessageUser),
     }))
-
     const response = await this.sendMessage(newMessages[0].text);
+
+
     const chatMessage = [
       {
         _id: Math.random().toString(36).substring(7),
@@ -93,7 +226,7 @@ class HomeScreen extends React.Component {
         createdAt: new Date(),
         user: {
           _id: 2,
-          name: 'Bible Answer Man',
+          name: 'Bible Man',
           avatar: require('../assets/chatgpt.jpg'),
         },
       },
@@ -101,38 +234,21 @@ class HomeScreen extends React.Component {
     this.setState(previousState => ({
       messages: GiftedChat.append(previousState.messages, chatMessage),
     }))
-
+    let responsefilter = response.replaceAll(/Allah/g, "Al lah").replaceAll(/(\d+):(\d+)/g, "pasal $1 ayat $2")
+    if(this.state.sound)
+    {
+      Tts.speak(responsefilter);
+      console.log("Speak !")
+    }
+    
   };
-  renderSend(props) {
-    return (
-      <View style={{  flexDirection: "row" }}>
-        <Send
-          {...props}
-        >
-             <View style={{marginRight : 8, paddingTop:20}}>
-            <Avatar
-              size="small"
-              source={require('../assets/send.png')}
-            />
-          </View>
-        </Send>
-        <TouchableOpacity onPress={() => { Alert.alert("Masih dikembangkan !") }}>
-          <View style={{ marginLeft:8, marginRight : 8, paddingTop:12}}>
-            <Avatar
-              size="small"
-              source={require('../assets/voice.png')}
-            />
-          </View>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+
   render() {
     return (
       <Layout style={{ flex: 1 }}>
         <TopNavigation
           alignment='center'
-          title='Bible Answer Man'
+          title='Bible Man'
           subtitle='Christian AI ChatBot'
           accessoryLeft={(props) => (
             <React.Fragment>
@@ -144,33 +260,110 @@ class HomeScreen extends React.Component {
             </React.Fragment>
 
           )}
-          accessoryRight={(props) => (
-            <React.Fragment>
-              <TouchableOpacity>
-                <Icon
-                  style={styles.icon}
-                  fill='#8F9BB3'
-                  name='settings-2-outline'
+          accessoryRight={() => (
+            <>
+              <OverflowMenu
+                anchor={() => <TopNavigationAction
+                  icon={() => <Icon
+                    style={styles.icon}
+                    fill='#8F9BB3'
+                    name='settings-2-outline'
+                  />}
+                  onPress={() => { this.setState({ menuVisible: !this.state.menuVisible }) }}
+                />}
+                visible={this.state.menuVisible}
+                onBackdropPress={() => { this.setState({ menuVisible: !this.state.menuVisible }) }}
+              >
+                <MenuItem
+                  accessoryLeft={() => (
+                    <MenuItem
+                      accessoryLeft={() => <Icon
+                        style={styles.icon}
+                        fill='#8F9BB3'
+                        name='volume-up-outline'
+                      />}
+                      title='Sound On'
+                      onPress={()=>{
+                        Alert.alert("Sound On", "Suara Bible Man dinyalakan")
+                        this.setState({sound: true})
+                      }}
+                    />
+                  )}
                 />
-              </TouchableOpacity>
-            </React.Fragment>
+                <MenuItem
+                  accessoryLeft={() => (
+                    <MenuItem
+                      accessoryLeft={() => <Icon
+                        style={styles.icon}
+                        fill='#8F9BB3'
+                        name='volume-off-outline'
+                      />}
+                      title='Sound Off'
+                      onPress={()=>{
+                        Alert.alert("Sound Off", "Suara Bible Man dimatikan")
+                        this.setState({sound: false})
+                      }}
+                    />
+                  )}
+                />
+              </OverflowMenu>
+            </>
           )}
         />
         <Divider />
-          <GiftedChat
-            style={{ flexDirection: "column-reverse"}}
-            messages={this.state.messages}
-            onSend={messages => this.onSend(messages)}
-            renderSend={this.renderSend}
-            user={user}
-            bottomOffset={-20}
-            alwaysShowSend={true}
-            placeholder={'Silahkan tanya Bible Answer Man : '}
-            showUserAvatar={true}
-            showAvatarForEveryMessage={true}
-            renderInputToolbar={(props) => (<InputToolbar {...props} containerStyle={styles.input} />)}
-            messagesContainerStyle={styles.messageContainer}
-          />
+        <GiftedChat
+          style={{}}
+          messages={this.state.messages}
+          onSend={messages => { this.onSend(messages) }}
+          renderSend={(props) => (<View style={{ flexDirection: "row" }}>
+            <Send
+              {...props}
+            >
+              <View style={{ marginRight: 8, paddingTop: 20 }}>
+                <Avatar
+                  size="small"
+                  source={require('../assets/send.png')}
+                />
+              </View>
+            </Send>
+            <TouchableOpacity onPress={async () => {
+              Tts.stop();
+              await this._StartRecognizing()
+            }}>
+              <View style={{ marginLeft: 8, marginRight: 8, paddingTop: 12 }}>
+                <Avatar
+                  size="small"
+                  source={require('../assets/voice.png')}
+                />
+              </View>
+            </TouchableOpacity>
+          </View>)}
+          user={user}
+          bottomOffset={-10}
+          renderLoading={() => {
+            return (
+              <Spinner size="tiny" />
+            );
+          }}
+          renderFooter={() => {
+            if (this.state.isTyping) {
+              return (<View style={{ justifyContent: "center", alignItems: "center" }}>
+                <Spinner size="tiny" /></View>
+              );
+            }
+          }}
+          wrapInSafeArea={false}
+          alwaysShowSend={true}
+          placeholder={'Silahkan tanya Bible Man : '}
+          showUserAvatar={true}
+          renderUsernameOnMessage={true}
+          showAvatarForEveryMessage={true}
+          renderInputToolbar={(props) => (<InputToolbar placeholderTextColor="#000" {...props} containerStyle={styles.input} />)}
+          messagesContainerStyle={styles.messageContainer}
+          shouldUpdateMessage={() => {
+            return true;
+          }}
+        />
 
       </Layout>
     )
@@ -181,10 +374,10 @@ class HomeScreen extends React.Component {
     paddingBottom: 16,
   },
   input: {
-    borderColor: 'transparent',
     borderWidth: 0,
     borderRadius: 4,
-    marginBottom: 16,
+    marginBottom: 8,
+    color: "black"
   },
   container: {
     flex: 1,
